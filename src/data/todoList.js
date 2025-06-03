83a786a444d73c950b9b7b6263559cdaace95bc7
fady <fadyyoussef4444@gamil.com>
2025-06-03 21:39:13 +0300

    feat: create static data for card componet

---
+++ src/data/todoList.js
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+export const ToDoList = [
+  {
+    id: uuidv4(),
+    title: "first ToDo",
+    detail: "first ToDo",
+    isCompeleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "second ToDo",
+    detail: "second ToDo",
+    isCompeleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "third ToDo",
+    detail: "third ToDo",
+    isCompeleted: false,
+  },
+];
